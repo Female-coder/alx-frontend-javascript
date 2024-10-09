3c55565ffd0a907cbcf0247a35363f814a041cdd
Female-coder <magadanineo@gmail.com>
2024-10-09 23:03:29 +0200

    ES6-classes

---
+++ 0x02-ES6_classes/babel.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+	rts = {
+		  presets: [
+			      [
+				            '@babel/preset-env',
+				            {
+						            targets: {
+								              node: 'current',
+								            },
+						          },
+				          ],
+			    ],
+	};
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+};
